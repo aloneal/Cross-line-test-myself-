f5600175c1bbe13e2d15b1f3e44f272cb836bbe0
aloneal <mengreyelf@yandex.ru>
2016-04-05 17:39:41 +0300

    Added files via upload

--- find_cross_line.py
+++ find_cross_line.py
@@ -1,5 +1,5 @@
 import sympy.geometry as symg
-from random import randint as rand
+import random
 
 
 def findcross(one_line, second_line):
@@ -10,11 +10,16 @@ def findcross(one_line, second_line):
     p1, p2, p3, p4 = symg.Point(x1, y1), symg.Point(x2, y2), symg.Point(x3, y3), symg.Point(x4, y4)
     l1, l2 = symg.Segment(p1, p2), symg.Segment(p3, p4)
     pointxy = symg.intersection(l1, l2)
-    if pointxy != []:
-        pointxy = [round(pointxy[0].x, 4), round(pointxy[0].y, 4)]
-    return pointxy
 
-# print(findcross([[1, 0], [1, 1.01]], [[0, 0], [2, 1.33]]))
+    if pointxy:
+        pointxy = [pointxy[0].x, pointxy[0].y]
+    """ кажется, что if работает быстрее
+    try:
+        pointxy = [pointxy[0].x, pointxy[0].y]
+    except IndexError:
+        return pointxy
+    """
+    return pointxy
 
 
 def chose_poit(point_index_f, pointsall_f, lines_all_f):
@@ -22,18 +27,35 @@ def chose_poit(point_index_f, pointsall_f, lines_all_f):
     pointsall_f - список всех точек
     выбор следующей точки по критерию уникальности новой линии, возвращает точку [x,y]"""
     one_more = 1
+    next_point1_f = []
+    rand_range = list(range(len(pointsall_f) - 1))  #.remove(point_index_f)
+    print(rand_range)
+
     while one_more == 1:
-        chose1_f = point_index_f
+        chose1_f = random.choice(rand_range)
+
         while chose1_f == point_index_f:
-            chose1_f = rand(0, len(pointsall_f) - 1)  # чтобы не было совпадений с точкой начала
-        next_point1_f = pointsall_f[chose1_f]  # выбор точки, куда потянеться линя из первой точки
+            # чтобы не было совпадений с точкой начала
+            rand_range.remove(chose1_f)
+            chose1_f = random.choice(rand_range)
+
+        # выбор точки, куда потянеться линя из первой точки
+        next_point1_f = pointsall_f[chose1_f]
+
+        # создание линии для проверки на совпадение
         p01 = symg.Point(pointsall_f[point_index_f][0], pointsall_f[point_index_f][1])
-        p02 = symg.Point(next_point1_f[0], next_point1_f[1])  # создание линии для проверки на совпадение
+        p02 = symg.Point(next_point1_f[0], next_point1_f[1])
         one_more = 0
+
         for j in range(len(lines_all_f) - 1):
             p1 = symg.Point(lines_all_f[j][0], lines_all_f[j][1])
             p2 = symg.Point(lines_all_f[j + 1][0], lines_all_f[j + 1][1])
-            if symg.Point.is_collinear(p1, p2, p01, p02) == True:
+
+            # проверка на коллинеарность из-за возможных наложений линий при несовпадении их длин, начала и конца
+            if symg.Point.is_collinear(p1, p2, p01, p02):
                 one_more = 1
+                # удалить из списка неудачную точку
+                rand_range.remove(chose1_f)
                 break
+
     return next_point1_f
--- main1.py
+++ main1.py
@@ -1,55 +1,66 @@
-import math  # математические функции
+import numpy as np
 import matplotlib.pyplot as plt
-import graphic1  # 32 строка
+import matplotlib.animation as anima
 import find_cross_line as fcr
 from random import randint as rand
 
 # start_n = int(input("начальное число вершин от 5 до 10:"))
-start_n = 8
+start_n = 7
+
 if start_n < 5:
     start_n = 5
+
 if start_n > 10:
     start_n = 10
+
 print("начальное число вершин", start_n)
 
-start_m = 20
+start_m = 10
 # start_m = int(input("число итераций от 5 до 20:"))
 print("число итераций", start_m)
 
-pointsall = []  # объявляю пустой список
+pointsall = []
 
-for i in range(start_n):  # создание списка координат вершин многогранника
-    pointsall = pointsall + \
-                [[round(math.sin(2 * math.pi * i / start_n), 4), round(math.cos(2 * math.pi * i / start_n), 4)]]
+# создание списка координат вершин многогранника
+for i in range(start_n):
+    pointsall = pointsall + [[np.sin(2 * np.pi * i / start_n), np.cos(2 * np.pi * i / start_n)]]
 
-linesall = pointsall + [pointsall[0]]  # список линий включает повторяющееся точки, а список точек - нет
-print(pointsall, "все линии:", linesall, sep='\n')
+# список линий включает повторяющееся точки, а список точек - нет
+linesall = pointsall + [pointsall[0]]
+# print(pointsall, "все линии:", linesall, sep='\n')
 
 """Для инициации процесса первая линия проводиться вручную,
 чтобы точка начала и конца не вопали в выбор для новой линии"""
 
-chose1 = rand(2, len(pointsall) - 2)  # первая итерация на многограннике исключает начальную точку и две рядом
-next_point1 = pointsall[chose1]  # выбор точки, куда потянеться линя из первой точки
-# next_line = [pointsall[0], next_point]  # создание линии для проверки на совпадение
-print("первая случайная:", chose1, "длина всех точек минус 1:", len(pointsall) - 1)
-linesall = linesall + [next_point1]  # дополнене списка линий еще одной
-print("все точки:", pointsall, "все линии:", linesall, sep='\n')
+# первая итерация на многограннике исключает начальную точку и две рядом
+chose1 = rand(2, start_n - 2)
+# выбор точки, куда потянеться линя из первой точки
+next_point1 = pointsall[chose1]
+# дополнене списка линий еще одной
+linesall = linesall + [next_point1]
 
 """Для выбора второй точки надо учесть, что список точек не увеличился и надо исключить последнюю точку линий"""
 
-temppoints = pointsall[:]  #создание временного списка для второй линии
-# temppoints = [pointsall[1:chose1 - 1], pointsall[chose1 + 1:]]
-# del temppoints[0]
-temppoints.remove(pointsall[chose1 + 1])  #удаление точки к которой пришла линия и двух рядом
+# создание временного списка для второй линии
+temppoints = pointsall[:]
+
+# удаление точки к которой пришла линия и двух рядом
+temppoints.remove(pointsall[chose1 + 1])
 temppoints.remove(pointsall[chose1])
 temppoints.remove(pointsall[chose1 - 1])
 temppoints.remove(pointsall[0])
-chose2 = rand(0, len(temppoints) - 1)  # первая итерация на многограннике исключает начальную точку и следующую
-next_point2 = temppoints[chose2]  # выбор точки, куда потянется линия из первой точки
-linesall = linesall + [next_point2]  # дополнене списка линий еще одной
-point_index = pointsall.index(next_point2)  #индекс для передачи в функцию поиска новой линии
-print(point_index, "список точек 2:", temppoints)
-print("все точкии:", pointsall, "все линии:", linesall, sep='\n')
+
+# первая итерация на многограннике исключает начальную точку и следующую
+chose2 = rand(0, len(temppoints) - 1)
+
+# выбор точки, куда потянется линия из первой точки
+next_point2 = temppoints[chose2]
+
+# дополнене списка линий еще одной
+linesall = linesall + [next_point2]
+
+# индекс для передачи в функцию поиска новой линии
+point_index = pointsall.index(next_point2)
 temppoints.clear()
 
 """после построения двух линий дальнейшие действия ведутся через функцию"""
@@ -58,13 +69,19 @@ temppoints.clear()
 def finde_all_cross(linesall_f):
     """передать список всех линий,
     поиск имеет смысл начинать с эн+2-й линии"""
-    next_line_f = [linesall_f[-2], linesall_f[-1]]  # последняя линия
+
+    # последняя линия
+    next_line_f = [linesall_f[-2], linesall_f[-1]]
     point_cross_f = []
     """разобраться с range(start_n, len(linesall_f) - 2) - чтобы работал без ошибок"""
-    for k in range(start_n, len(linesall_f) - 2):  #перебор по очереди всех линий с эн и без 2 последних пересечения нет
+
+    # перебор по очереди всех линий с эн и без 2 последних пересечения нет
+    for k in range(start_n, len(linesall_f) - 2):
         add_cross = [fcr.findcross(next_line_f, [linesall_f[k], linesall_f[k + 1]])]
+
         if add_cross != [[]]:
             point_cross_f = point_cross_f + add_cross
+
     return point_cross_f
 
 # print((next_point in pointsall))  #элемент есть в списке
@@ -74,17 +91,18 @@ for j in range(start_m - 2):
     next_point = fcr.chose_poit(point_index, pointsall, linesall)
     print("следующая точка", next_point)
     point_index = pointsall.index(next_point)
-    print("индекс", point_index)
+    # print("индекс", point_index)
     linesall = linesall + [next_point]
-    print("все линии", linesall)
+    # print("все линии", linesall)
     point_cross = finde_all_cross(linesall)
-    print("cross:", point_cross)
+    # print("cross:", point_cross)
 
-    for j in range(len(point_cross)):
-        check = 0
-        if pointsall.count(point_cross[j]) == 0:
-            pointsall = pointsall + [point_cross[j]]
-    print("все точки", pointsall)
+    for k in range(len(point_cross)):
+        if pointsall.count(point_cross[k]) == 0:
+            pointsall = pointsall + [point_cross[k]]
+
+    # pointsall = pointsall + point_cross
+    # print("все точки", pointsall)
 
 print("все линии", linesall)
 pointsx = []
@@ -94,9 +112,8 @@ for i in range(len(linesall)):
     pointsx = pointsx + [linesall[i][0]]
     pointsy = pointsy + [linesall[i][1]]
 
-plt.plot(pointsx, pointsy)  #нарисовать линии по списку координат
-# graphic1.save("picture1")  #сохранение картинки на диск
 """for i in range(len(pointsx)):  #подписи координат к вершинам
     plt.text(pointsx[i], pointsy[i], str([pointsx[i], pointsy[i]]))"""
-plt.axis([-2, 2, -2, 2])  #размер поля для вывода графика
-plt.show()  #показать окно с графикой
+plt.axis([-2, 2, -2, 2])
+ani = anima.ArtistAnimation(plt.figure(), plt.plot(pointsx, pointsy), interval=50, blit=True, repeat_delay=1000)
+plt.show()
